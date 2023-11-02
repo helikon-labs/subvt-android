00ed19d6822a70358b5dbd4c4c20c2cf1af0a8cf
kukabi <kutsalbilgin@gmail.com>
2023-11-02 16:24:56 +0300

    Switch to JetPack Compose.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
+        maven("https://jitpack.io")
     }
 }
+
 rootProject.name = "SubVT"
-include ':subvt'
+include(":subvt")
+ 
