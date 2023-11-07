5bdcf94b8439785c89bad22682d20ed311ce194d
kukabi <kutsalbilgin@gmail.com>
2023-11-07 18:26:16 +0300

    Project setup continued. Onboarding.

--- subvt/src/main/java/io/helikon/subvt/ui/theme/Color.kt
+++ subvt/src/main/java/io/helikon/subvt/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package io.helikon.subvt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Dark = Color(0xFF041A25)
+val Light = Color(0xFFF5F5F5)
+val Blue = Color(0xFF3A6DFF)
+val DarkBlue = Color(0xFF1D53EE)
--- subvt/src/main/java/io/helikon/subvt/ui/theme/Type.kt
+++ subvt/src/main/java/io/helikon/subvt/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package io.helikon.subvt.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.helikon.subvt.R
+
+val lexendDecaFamily = FontFamily(
+    Font(R.font.lexend_deca_black, FontWeight.Black),
+    Font(R.font.lexend_deca_bold, FontWeight.Bold),
+    Font(R.font.lexend_deca_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.lexend_deca_extra_light, FontWeight.ExtraLight),
+    Font(R.font.lexend_deca_light, FontWeight.Light),
+    Font(R.font.lexend_deca_medium, FontWeight.Medium),
+    Font(R.font.lexend_deca_regular, FontWeight.Normal),
+    Font(R.font.lexend_deca_semi_bold, FontWeight.SemiBold),
+    Font(R.font.lexend_deca_thin, FontWeight.Thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineMedium = TextStyle(
+        fontFamily = lexendDecaFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    bodyMedium = TextStyle(
+        fontFamily = lexendDecaFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = lexendDecaFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+    ),
 )
